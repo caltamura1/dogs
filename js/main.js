6ca72393c52a7367e2fe8783d63e1463bb93bc78
caltamura1 <christian@tallwalltech.com>
2025-01-29 14:28:41 -0500

    adding js and faq section

---
+++ js/main.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const faqContainer = document.querySelector('.faq-content');
+
+    faqContainer.addEventListener('click', (e) => {
+        const groupHeader = e.target.closest('.faq-group-header');
+
+        if(!groupHeader) return;
+
+        const group = groupHeader.parentElement;
+        const groupBody = group.querySelector('.faq-group-body');
+        const icon = groupHeader.querySelector('i');
+
+        //Toggle Icon
+        icon.classList.toggle('fa-plus');
+        icon.classList.toggle('fa-minus');
+
+        //Toggle Visisblity of Body
+        groupBody.classList.toggle('open')
+
+        console.log(groupHeader)
+    })
+})
